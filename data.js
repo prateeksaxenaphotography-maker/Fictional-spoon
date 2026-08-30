9c4f603e826dbdb54d2c364bdf49394d229abbb2
prateeksaxenaphotography-maker <prateeksaxenaphotography@gmail.com>
2026-08-30 11:12:58 +0530

    Auto-sync portfolio data from Admin Panel

--- data.js
+++ data.js
@@ -1609,7 +1609,7 @@ window.WPS_DATA = {
       "venueCost": null
     },
     {
-      "code": "NERDYHOME",
+      "code": "NERDYHOMECOMP",
       "desc": "Photographer direct unlock code",
       "location": "Home Studio, Sector 46, Noida",
       "venueCost": null
@@ -1620,8 +1620,8 @@ window.WPS_DATA = {
       "location": "Noida, Sector-46",
       "venueCost": 4000,
       "homeStudioDiscount": {
-        "type": "flat",
-        "value": 500
+        "type": "pct",
+        "value": 10
       }
     }
   ],
