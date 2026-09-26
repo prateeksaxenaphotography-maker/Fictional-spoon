a4180e614088b3876ea7a891b61a30a95749d4c1
prateeksaxenaphotography-maker <prateeksaxenaphotography@gmail.com>
2026-09-26 17:10:25 +0530

    Auto-sync portfolio data from Admin Panel

--- data.js
+++ data.js
@@ -2898,8 +2898,8 @@ window.WPS_DATA = {
     ],
     "paymentScheduleType": "5050",
     "productionScheduleType": "503020",
-    "updatedAt": 1790422796948,
-    "syncedAt": 1790422796948
+    "updatedAt": 1790422820173,
+    "syncedAt": 1790422820173
   },
   "INVITE_CODES": [
     {
@@ -3097,7 +3097,7 @@ window.WPS_DATA = {
     "PACKAGES": 1790257919972,
     "TFP_PACKAGE": 1790257919972,
     "HOME_STUDIO_RATE": 1789975069138,
-    "PORTFOLIO_PDF": 1790422792000,
+    "PORTFOLIO_PDF": 1790422802684,
     "HOME_SLIDESHOW": 1790354674280,
     "MEASURE_UNITS": 1790269147688,
     "HOME_STUDIO_RATE_TFP": 1789975069138
@@ -3844,116 +3844,13 @@ window.WPS_DATA = {
             }
           }
         }
-      },
-      {
-        "id": "mp_mueija7w_ny3rh",
-        "shootId": "comp-card-Atharv%20Sharma%20(https%3A%2F%2Fwww.instagram.com%2Fectogainsss%2F%3Fhl%3Den)",
-        "name": "Test 1",
-        "updatedAt": 1790422773967,
-        "spec": {
-          "pages": 2,
-          "count": 8,
-          "perPage": [
-            4,
-            4
-          ],
-          "span": {
-            "muca80smjx9k90-18": 2
-          },
-          "firstPage": 0,
-          "picks": [
-            "muca5877bysjsu-4",
-            "muca56vgez53vb-0",
-            "muca57yixr16s8-3",
-            "muca57hap5pcdi-1",
-            "muccmrnumyyf3j-23",
-            "muca593bwa9nvo-6",
-            "muca80xnhe2eom-19",
-            "muca80smjx9k90-18"
-          ],
-          "cleared": [],
-          "lead": "muca80smjx9k90-18",
-          "cover": true,
-          "coverId": "muca5a1qkkj6qh-11",
-          "coverStyle": "framed",
-          "tags": [
-            true,
-            false
-          ],
-          "tagPlace": "below",
-          "tagAlign": "left",
-          "layout": "equal",
-          "layouts": [
-            "equal",
-            "lead"
-          ],
-          "cols": [
-            0,
-            0
-          ],
-          "bigAt": [
-            "",
-            "top"
-          ],
-          "detailsAlign": "centre",
-          "statsAlign": "centre",
-          "contactAlign": "left",
-          "spacing": "none",
-          "order": [
-            "muca56vgez53vb-0",
-            "muca5877bysjsu-4",
-            "muca57hap5pcdi-1",
-            "muca57yixr16s8-3",
-            "muca80smjx9k90-18",
-            "muca80xnhe2eom-19",
-            "muca593bwa9nvo-6",
-            "muccmrnumyyf3j-23"
-          ],
-          "fewerOnTop": false,
-          "adjust": {
-            "muca80smjx9k90-18": {
-              "x": 0.5,
-              "y": 0.08963994902161698,
-              "zoom": 1
-            },
-            "muccmrnumyyf3j-23": {
-              "x": 0.5,
-              "y": 0.35,
-              "zoom": 1
-            },
-            "muca593bwa9nvo-6": {
-              "x": 0.5,
-              "y": 0.35,
-              "zoom": 1
-            },
-            "muca80xnhe2eom-19": {
-              "x": 0.18603887524945684,
-              "y": 0.24573177002982619,
-              "zoom": 1
-            },
-            "muca57hap5pcdi-1": {
-              "x": 0.5,
-              "y": 1,
-              "zoom": 1
-            },
-            "muca5877bysjsu-4": {
-              "x": 0.5,
-              "y": 0.6092267115868704,
-              "zoom": 1
-            },
-            "muca57yixr16s8-3": {
-              "x": 0.5,
-              "y": 0.6395129213251747,
-              "zoom": 1
-            }
-          }
-        }
       }
     ],
     "deleted": [
       "mp_mucczfy9_wugha",
       "mp_mucz851x_82fju",
-      "mp_mud2vlkr_co23k"
+      "mp_mud2vlkr_co23k",
+      "mp_mueija7w_ny3rh"
     ]
   },
   "STUDIO_PORTFOLIOS": {
